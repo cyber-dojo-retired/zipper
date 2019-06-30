3a5e67bfe672c76876d719b0f790354dd807f0b1
Jon Jagger <jon@jaggersoft.com>
2019-06-30 12:50:45 +0100

    tar pipe test data into /cyber-dojo tmpfs in saver service

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -5,5 +5,6 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
+"${SH_DIR}/copy_in_saver_test_data.sh" 2> /dev/null
 "${SH_DIR}/run_tests_in_containers.sh" "$@"
 "${SH_DIR}/docker_containers_down.sh"
